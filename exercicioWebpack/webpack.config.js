f6e5dd3012d7767aa954082577daadd4f52d8aed
Gabriel Silva <63379300+GABRIEL08266644@users.noreply.github.com>
2021-02-20 14:51:23 -0300

    ex webpack

---
+++ exercicioWebpack/webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const ModoDev = process.env.NODE_ENV !== 'production'
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+
+module.exports = {
+    mode: ModoDev ? 'development' : 'production',
+    entry: './src/principal.js',
+    output: {   
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    // optimization: {
+    //     // minimizer: [
+    //     //     new UglifyJsPlugin({
+    //     //         cache: true,     
+    //     //         parallel: true
+    //     //     }),
+    //         // new OptimizeCSSAssetsPlugin({})
+    //     // ]
+    // },
+    plugins: [
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        }),
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                // 'style-loader',  // add css a DOM 
+                'css-loader', // imperpreta url
+                'sass-loader',
+            ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
+        }]
+    }
+    // module: {
+    //     rules: [{
+    //         test: /\.css$/,
+    //         use: [
+    //             MiniCssExtractPlugin.loader,
+    //             // 'style-loader',  // add css a DOM 
+    //             'css-loader' // imperpreta url
+    //         ]
+    //     }]
+    // }
+}
