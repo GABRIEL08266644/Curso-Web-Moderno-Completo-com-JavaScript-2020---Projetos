6c8a7683cc2ef716adc667c1cf76e625b57eb36c
Gabriel Silva <63379300+GABRIEL08266644@users.noreply.github.com>
2021-02-23 22:06:20 -0300

    webpack completo

--- exercicioWebpack/webpack.config.js
+++ exercicioWebpack/webpack.config.js
@@ -1,44 +1,41 @@
-const ModoDev = process.env.NODE_ENV !== 'production'
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
-
 module.exports = {
-    mode: ModoDev ? 'development' : 'production',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
-    output: {   
+    output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
-    // optimization: {
-    //     // minimizer: [
-    //     //     new UglifyJsPlugin({
-    //     //         cache: true,     
-    //     //         parallel: true
-    //     //     }),
-    //         // new OptimizeCSSAssetsPlugin({})
-    //     // ]
-    // },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: [
-        new TerserPlugin({
-            parallel: true,
-            terserOptions: {
-                ecma: 6,
-            },
-        }),
         new MiniCssExtractPlugin({
             filename: "estilo.css"
-        }),
+        })
     ],
     module: {
         rules: [{
             test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
-                // 'style-loader',  // add css a DOM 
-                'css-loader', // imperpreta url
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                'css-loader', // interpreta @import, url()...
                 'sass-loader',
             ]
         }, {
@@ -46,14 +43,4 @@ module.exports = {
             use: ['file-loader']
         }]
     }
-    // module: {
-    //     rules: [{
-    //         test: /\.css$/,
-    //         use: [
-    //             MiniCssExtractPlugin.loader,
-    //             // 'style-loader',  // add css a DOM 
-    //             'css-loader' // imperpreta url
-    //         ]
-    //     }]
-    // }
 }
